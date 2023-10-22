fe6a34bc7e000b15cd99884ac497fe1146d3c1ce
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-10-22 15:24:12 +0200

    setup order table skel on create.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,27 +4,62 @@
 
 @section('content')
 <div class="container">
-    <h1>Create a New Gift</h1>
-    <form method="POST" action="{{ route('gifts.store') }}">
-        @csrf
-        <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" required>
+    <div class="row">
+        <div class="col-6 p-4 bg-success">
+            <h1>Create a New Gift</h1>
+                <form method="POST" action="{{ route('gifts.store') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="lead" class="form-label">Lead</label>
+                        <input type="text" class="form-control" id="lead" name="lead" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea class="form-control" id="content" name="content" rows="4" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="tags" class="form-label">Tags</label>
+                        <input type="text" class="form-control" id="tags" name="tags" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Gift</button>
+                </form>
         </div>
-        <div class="mb-3">
-            <label for="lead" class="form-label">Lead</label>
-            <input type="text" class="form-control" id="lead" name="lead" required>
+        <div class="col-6 p-4 bg-success">
+            <h1>Orders</h1>
+
+            <table class="table table-striped table-dark table-responsive">
+                <thead>
+                  <tr>
+                    <th>Header 1</th>
+                    <th>Header 2</th>
+                    <th>Header 3</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Data 1-1</td>
+                    <td>Data 1-2</td>
+                    <td>Data 1-3</td>
+                  </tr>
+                  <tr>
+                    <td>Data 2-1</td>
+                    <td>Data 2-2</td>
+                    <td>Data 2-3</td>
+                  </tr>
+                  <tr>
+                    <td>Data 3-1</td>
+                    <td>Data 3-2</td>
+                    <td>Data 3-3</td>
+                  </tr>
+                </tbody>
+              </table>
         </div>
-        <div class="mb-3">
-            <label for="content" class="form-label">Content</label>
-            <textarea class="form-control" id="content" name="content" rows="4" required></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="tags" class="form-label">Tags</label>
-            <input type="text" class="form-control" id="tags" name="tags" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Create Gift</button>
-    </form>
+    </div>
+    
 </div>
 
 
